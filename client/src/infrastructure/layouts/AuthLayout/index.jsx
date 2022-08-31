069113f4fc3ab042d2047ca56297030c8bcab76d
Praveen Kumar <praveenkumarofficials@gmail.com>
2022-08-31 17:32:05 +0530

    prevented going to auth page when user is already signedin

--- client/src/infrastructure/layouts/AuthLayout/index.jsx
+++ client/src/infrastructure/layouts/AuthLayout/index.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../../services/auth/auth.context";
 
 export default function AuthLayout() {
+  const navigate = useNavigate();
+  const { authState } = useAuthContext();
+  const { profileFetchRequestCompleted, user } = authState;
+
+  useEffect(() => {
+    if (profileFetchRequestCompleted && user) {
+      navigate(-1, { replace: true });
+    }
+  }, [profileFetchRequestCompleted, user, navigate]);
+
   return (
     <div>
       <Outlet />
